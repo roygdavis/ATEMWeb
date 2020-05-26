85171f923883dd155ea588b8ca8fbc0b03e3de47
Roy Davis <Roy.Davis@mountainwarehouse.com>
2020-05-26 15:28:43 +0100

    Update nuget packages

--- ATEMWeb/Areas/HelpPage/HelpPageAreaRegistration.cs
+++ ATEMWeb/Areas/HelpPage/HelpPageAreaRegistration.cs
@@ -15,12 +15,12 @@ namespace ATEMWeb.Areas.HelpPage
 
         public override void RegisterArea(AreaRegistrationContext context)
         {
-         /*   context.MapRoute(
+            context.MapRoute(
                 "HelpPage_Default",
                 "Help/{action}/{apiId}",
                 new { controller = "Help", action = "Index", apiId = UrlParameter.Optional });
 
-            HelpPageConfig.Register(GlobalConfiguration.Configuration);*/
+            HelpPageConfig.Register(GlobalConfiguration.Configuration);
         }
     }
 }
