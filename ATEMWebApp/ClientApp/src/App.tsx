184028f7d3effdb0dc0fde0d013889567a4245bc
Roy G Davis <roygdavis@gmail.com>
2022-01-13 23:00:08 +0000

    SImple UI stuff

--- ATEMWebApp/ClientApp/src/App.tsx
+++ ATEMWebApp/ClientApp/src/App.tsx
@@ -8,7 +8,7 @@ interface AppState {
   messages: string[];
 }
 
-export default class App extends Component<{},AppState> {
+export default class App extends Component<{}, AppState> {
   static displayName = App.name;
 
   constructor(props: AppState) {
@@ -42,6 +42,17 @@ export default class App extends Component<{},AppState> {
           <p>As this is a dotnetcore app, it should be possible to run this as a server in a docker container, with WINE (for the Switchers API DLL) but I've not tested or even begun to test that yet</p>
         </div>
         <div className='row'>
+          <div className='col-4'>
+            <button type="button" className="btn btn-primary" data-bs-toggle="button" onClick={() => this.connect()}>Connect</button>
+          </div>
+          <div className='col-8'>
+            <div className="input-group mb-3">
+              <input type="text" className="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2" />
+              <button className="btn btn-outline-secondary" type="button" id="button-addon2" onClick={()=> this.setPgm(3)}>Button</button>
+            </div>
+          </div>
+        </div>
+        <div className='row'>
           <div className='col'>
             {messages?.map(x => <p>{x}</p>)}
           </div>
@@ -57,5 +68,15 @@ export default class App extends Component<{},AppState> {
     connection.on("connected", (pgmId: number) => {
       this.setState({ messages: [...this.state.messages, "connected"] });
     });
+  }
+
+  connect = async () => {
+    const response = await fetch("/atem/connect?address=");
+    const result = await response.text();
+  }
+
+  setPgm = async (pgmId: number) => {
+    const response = await fetch(`/atem/mixeffects/0/pgm/${pgmId}`, { method: 'POST' });
+    const result = await response.text();
   }
 }
